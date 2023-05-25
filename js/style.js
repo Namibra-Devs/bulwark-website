ee6d0a4bf05404143e1eeaa249d1be927d3fa7cd
Mohammed Adam <adammohammed.amd@gmail.com>
2023-05-25 22:41:10 +0000

    first commit

--- js/style.js
+++ js/style.js
@@ -14,7 +14,31 @@ toggleBtn.addEventListener("click", (e) => {
         navList.classList.remove("navbar-list-show");
     }
 })
+// const btns = document.querySelectorAll(".question-btn"); 
+// btns.forEach((btn) => {
+//  btn.addEventListener("click", (e) => {
+//   const question = e.currentTarget.parentElement.parentElement;
+//   question.classList.toggle('show-text')
+//  })
+// })
 
+const questions= document.querySelectorAll(".question"); 
+
+questions.forEach((question) => {
+const btn= question.querySelector(".question-btn"); 
+
+     btn.addEventListener("click", () => {
+      questions.forEach((item) => {
+        if(item !== question) {
+           item.classList.remove("show-text");
+        }
+      });
+      question.classList.toggle("show-text");
+
+     });
+     });
+
+    
 AOS.init({
     offSet:400,
     duration:1000
