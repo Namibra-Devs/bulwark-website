732e363c8c037d87024225440fe75b4bd3de04c5
Charles Owusu Bih <charlesowusubih@gmail.com>
2023-07-20 15:57:53 +0000

    changed title

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -32,7 +32,7 @@ $cur_page = substr($_SERVER["SCRIPT_NAME"], strrpos($_SERVER["SCRIPT_NAME"], "/"
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>JM Street-Life Studios - Admin Panel</title>
+	<title>Spendy's Bulwark - Admin Panel</title>
 
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
